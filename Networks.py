10eee517d67908220f23dce0c9cb1291dfb62b60
John Doe <nickgray0@gmail.com>
2022-06-11 17:31:47 -0700

    new

--- Networks.py
+++ Networks.py
@@ -1,7 +1,7 @@
 import math
-import numpy
 import torch
 import torch.nn as nn
+from Resamplers import InterpolativeUpsampler, InterpolativeDownsampler, InplaceUpsampler, InplaceDownsampler
 
 def MSRInitializer(Layer, ActivationGain=1):
     FanIn = Layer.weight.data.size(1) * Layer.weight.data[0][0].numel()
@@ -14,6 +14,7 @@ def MSRInitializer(Layer, ActivationGain=1):
 
 class BiasedActivation(nn.Module):
     Gain = math.sqrt(2)
+    Function = nn.functional.silu
     
     def __init__(self, InputUnits, ConvolutionalLayer=True):
         super(BiasedActivation, self).__init__()
@@ -25,7 +26,7 @@ class BiasedActivation(nn.Module):
         
     def forward(self, x):
         y = x + self.Bias.view(1, -1, 1, 1) if self.ConvolutionalLayer else x + self.Bias.view(1, -1)
-        return nn.functional.silu(y)
+        return BiasedActivation.Function(y)
 
 class GeneratorBlock(nn.Module):
       def __init__(self, InputChannels, CompressionFactor, ReceptiveField):
@@ -38,12 +39,11 @@ class GeneratorBlock(nn.Module):
           
           self.NonLinearity1 = BiasedActivation(CompressedChannels)
           self.NonLinearity2 = BiasedActivation(InputChannels)
-
+          
       def forward(self, x, ActivationMaps):
           y = self.LinearLayer1(ActivationMaps)
-          y = self.NonLinearity1(y)
+          y = self.LinearLayer2(self.NonLinearity1(y))
           
-          y = self.LinearLayer2(y)
           y = x + y
           
           return y, self.NonLinearity2(y)
@@ -66,38 +66,8 @@ class DiscriminatorBlock(nn.Module):
           
           return x + y
 
-def CreateLowpassKernel():
-    Kernel = numpy.array([[1., 2., 1.]])
-    Kernel = torch.Tensor(Kernel.T @ Kernel)
-    Kernel = Kernel / torch.sum(Kernel)
-    return Kernel.view(1, 1, Kernel.shape[0], Kernel.shape[1])
-
-class Upsampler(nn.Module):
-      def __init__(self):
-          super(Upsampler, self).__init__()
-          
-          self.register_buffer('Kernel', CreateLowpassKernel())
-          
-      def forward(self, x):
-          x = nn.functional.pixel_shuffle(x, 2)
-          y = nn.functional.pad(x, (1, 1, 1, 1), mode='reflect')
-          
-          return nn.functional.conv2d(y.view(y.shape[0] * y.shape[1], 1, y.shape[2], y.shape[3]), self.Kernel, stride=1).view(*x.shape)
-          
-class Downsampler(nn.Module):
-      def __init__(self):
-          super(Downsampler, self).__init__()
-          
-          self.register_buffer('Kernel', CreateLowpassKernel())
-          
-      def forward(self, x):
-          y = nn.functional.pad(x, (1, 1, 1, 1), mode='reflect')
-          y = nn.functional.conv2d(y.view(y.shape[0] * y.shape[1], 1, y.shape[2], y.shape[3]), self.Kernel, stride=1).view(*x.shape)
-
-          return nn.functional.pixel_unshuffle(y, 2)
-
 class GeneratorUpsampleBlock(nn.Module):
-      def __init__(self, InputChannels, OutputChannels, CompressionFactor, ReceptiveField):
+      def __init__(self, InputChannels, OutputChannels, CompressionFactor, ReceptiveField, ResamplingFilter):
           super(GeneratorUpsampleBlock, self).__init__()
           
           CompressedChannels = InputChannels // CompressionFactor
@@ -107,10 +77,12 @@ class GeneratorUpsampleBlock(nn.Module):
           self.LinearLayer3 = MSRInitializer(nn.Conv2d(CompressedChannels, OutputChannels, kernel_size=ReceptiveField, stride=1, padding=(ReceptiveField - 1) // 2, padding_mode='reflect', bias=False), ActivationGain=0)
           
           self.NonLinearity1 = BiasedActivation(CompressedChannels)
-          self.NonLinearity2 = BiasedActivation(CompressedChannels * 4)
+          self.NonLinearity2 = BiasedActivation(CompressedChannels)
           self.NonLinearity3 = BiasedActivation(OutputChannels)
           
-          self.Resampler = Upsampler()
+          self.MainResampler = InplaceUpsampler(ResamplingFilter)
+          self.ShortcutResampler = InterpolativeUpsampler(ResamplingFilter)
+          
           if InputChannels != OutputChannels:
               self.ShortcutLayer = MSRInitializer(nn.Conv2d(InputChannels, OutputChannels, kernel_size=1, stride=1, padding=0, bias=False))
 
@@ -119,16 +91,15 @@ class GeneratorUpsampleBlock(nn.Module):
               x = self.ShortcutLayer(x)
           
           y = self.LinearLayer1(ActivationMaps)
-          y = self.LinearLayer2(self.NonLinearity1(y))
-          y = self.Resampler(self.NonLinearity2(y))
+          y = self.MainResampler(self.LinearLayer2(self.NonLinearity1(y)))
+          y = self.LinearLayer3(self.NonLinearity2(y))
           
-          y = self.LinearLayer3(y)
-          y = nn.functional.interpolate(x, scale_factor=2, mode='bilinear', align_corners=False, antialias=False) + y
+          y = self.ShortcutResampler(x) + y
           
           return y, self.NonLinearity3(y)
 
 class DiscriminatorDownsampleBlock(nn.Module):
-      def __init__(self, InputChannels, OutputChannels, CompressionFactor, ReceptiveField):
+      def __init__(self, InputChannels, OutputChannels, CompressionFactor, ReceptiveField, ResamplingFilter):
           super(DiscriminatorDownsampleBlock, self).__init__()
           
           CompressedChannels = OutputChannels // CompressionFactor
@@ -138,31 +109,31 @@ class DiscriminatorDownsampleBlock(nn.Module):
           self.LinearLayer3 = MSRInitializer(nn.Conv2d(CompressedChannels, OutputChannels, kernel_size=ReceptiveField, stride=1, padding=(ReceptiveField - 1) // 2, padding_mode='reflect', bias=False), ActivationGain=0)
           
           self.NonLinearity1 = BiasedActivation(InputChannels)
-          self.NonLinearity2 = BiasedActivation(CompressedChannels * 4)
+          self.NonLinearity2 = BiasedActivation(CompressedChannels)
           self.NonLinearity3 = BiasedActivation(CompressedChannels)
           
-          self.Resampler = Downsampler()
+          self.MainResampler = InplaceDownsampler(ResamplingFilter)
+          self.ShortcutResampler = InterpolativeDownsampler(ResamplingFilter)
+          
           if InputChannels != OutputChannels:
               self.ShortcutLayer = MSRInitializer(nn.Conv2d(InputChannels, OutputChannels, kernel_size=1, stride=1, padding=0, bias=False))
           
       def forward(self, x):
           y = self.LinearLayer1(self.NonLinearity1(x))
+          y = self.LinearLayer2(self.MainResampler(self.NonLinearity2(y)))
+          y = self.LinearLayer3(self.NonLinearity3(y))
           
-          y = self.NonLinearity2(self.Resampler(y))
-          y = self.NonLinearity3(self.LinearLayer2(y))
-          y = self.LinearLayer3(y)
-          
-          x = nn.functional.interpolate(x, scale_factor=0.5, mode='bilinear', align_corners=False, antialias=True, recompute_scale_factor=True)
+          x = self.ShortcutResampler(x)
           if hasattr(self, 'ShortcutLayer'):
               x = self.ShortcutLayer(x)
 
           return x + y
      
 class GeneratorStage(nn.Module):
-      def __init__(self, InputChannels, OutputChannels, Blocks, CompressionFactor, ReceptiveField):
+      def __init__(self, InputChannels, OutputChannels, Blocks, CompressionFactor, ReceptiveField, ResamplingFilter):
           super(GeneratorStage, self).__init__()
           
-          self.BlockList = nn.ModuleList([GeneratorBlock(InputChannels, CompressionFactor, ReceptiveField) for _ in range(Blocks - 1)] + [GeneratorUpsampleBlock(InputChannels, OutputChannels, CompressionFactor, ReceptiveField)])
+          self.BlockList = nn.ModuleList([GeneratorBlock(InputChannels, CompressionFactor, ReceptiveField) for _ in range(Blocks - 1)] + [GeneratorUpsampleBlock(InputChannels, OutputChannels, CompressionFactor, ReceptiveField, ResamplingFilter)])
           
       def forward(self, x, ActivationMaps):
           for Block in self.BlockList:
@@ -170,10 +141,10 @@ class GeneratorStage(nn.Module):
           return x, ActivationMaps
           
 class DiscriminatorStage(nn.Module):
-      def __init__(self, InputChannels, OutputChannels, Blocks, CompressionFactor, ReceptiveField):
+      def __init__(self, InputChannels, OutputChannels, Blocks, CompressionFactor, ReceptiveField, ResamplingFilter):
           super(DiscriminatorStage, self).__init__()
 
-          self.BlockList = nn.ModuleList([DiscriminatorDownsampleBlock(InputChannels, OutputChannels, CompressionFactor, ReceptiveField)] + [DiscriminatorBlock(OutputChannels, CompressionFactor, ReceptiveField) for _ in range(Blocks - 1)])
+          self.BlockList = nn.ModuleList([DiscriminatorDownsampleBlock(InputChannels, OutputChannels, CompressionFactor, ReceptiveField, ResamplingFilter)] + [DiscriminatorBlock(OutputChannels, CompressionFactor, ReceptiveField) for _ in range(Blocks - 1)])
         
       def forward(self, x):
           for Block in self.BlockList:
@@ -250,7 +221,7 @@ def ToRGB(InputChannels, ResidualComponent=False):
     return MSRInitializer(nn.Conv2d(InputChannels, 3, kernel_size=1, stride=1, padding=0, bias=False), ActivationGain=0 if ResidualComponent else 1)
 
 class Generator(nn.Module):
-    def __init__(self, NoiseDimension=512, LatentDimension=512, LatentMappingDepth=8, PrologWidth=1024, StageWidths=[1024, 512, 512, 512, 256, 256, 256, 128], BlocksPerStage=[2, 2, 2, 2, 2, 2, 2, 2], CompressionFactor=4, ReceptiveField=3):
+    def __init__(self, NoiseDimension=512, LatentDimension=512, LatentMappingDepth=8, PrologWidth=1024, StageWidths=[1024, 512, 512, 512, 256, 256, 256, 128], BlocksPerStage=[4, 4, 4, 4, 4, 4, 4, 4], CompressionFactor=4, ReceptiveField=3, ResamplingFilter=[1, 2, 1]):
         super(Generator, self).__init__()
         
         self.LatentLayer = MappingBlock(NoiseDimension, LatentDimension, LatentMappingDepth // 2 - 1)
@@ -262,10 +233,11 @@ class Generator(nn.Module):
         AggregationLayers = []
         ExtendedStageWidths = [PrologWidth] + StageWidths
         for x in range(len(StageWidths)):
-            MainLayers += [GeneratorStage(ExtendedStageWidths[x], ExtendedStageWidths[x + 1], BlocksPerStage[x], CompressionFactor, ReceptiveField)]
+            MainLayers += [GeneratorStage(ExtendedStageWidths[x], ExtendedStageWidths[x + 1], BlocksPerStage[x], CompressionFactor, ReceptiveField, ResamplingFilter)]
             AggregationLayers += [ToRGB(ExtendedStageWidths[x + 1], ResidualComponent=True)]    
         self.MainLayers = nn.ModuleList(MainLayers)
         self.AggregationLayers = nn.ModuleList(AggregationLayers)
+        self.Resampler = InterpolativeUpsampler(ResamplingFilter)
 
     def forward(self, z, EnableLatentMapping=True):
         w = self.LatentLayer(z) if EnableLatentMapping else z
@@ -275,12 +247,12 @@ class Generator(nn.Module):
 
         for Layer, Aggregate in zip(self.MainLayers, self.AggregationLayers):
             y, ActivationMaps = Layer(y, ActivationMaps)
-            AggregatedOutput = nn.functional.interpolate(AggregatedOutput, scale_factor=2, mode='bilinear', align_corners=False, antialias=False) + Aggregate(ActivationMaps)
+            AggregatedOutput = self.Resampler(AggregatedOutput) + Aggregate(ActivationMaps)
         
         return AggregatedOutput
 
 class Discriminator(nn.Module):
-    def __init__(self, LatentDimension=512, EpilogWidth=1024, StageWidths=[128, 256, 256, 256, 512, 512, 512, 1024], BlocksPerStage=[2, 2, 2, 2, 2, 2, 2, 2], CompressionFactor=4, ReceptiveField=3):
+    def __init__(self, LatentDimension=512, EpilogWidth=1024, StageWidths=[128, 256, 256, 256, 512, 512, 512, 1024], BlocksPerStage=[4, 4, 4, 4, 4, 4, 4, 4], CompressionFactor=4, ReceptiveField=3, ResamplingFilter=[1, 2, 1]):
         super(Discriminator, self).__init__()
         
         self.FromRGB = MSRInitializer(nn.Conv2d(3, StageWidths[0], kernel_size=ReceptiveField, stride=1, padding=(ReceptiveField - 1) // 2, padding_mode='reflect', bias=False), ActivationGain=BiasedActivation.Gain)
@@ -288,7 +260,7 @@ class Discriminator(nn.Module):
         MainLayers = []
         ExtendedStageWidths = StageWidths + [EpilogWidth]
         for x in range(len(StageWidths)):
-            MainLayers += [DiscriminatorStage(ExtendedStageWidths[x], ExtendedStageWidths[x + 1], BlocksPerStage[x], CompressionFactor, ReceptiveField)]
+            MainLayers += [DiscriminatorStage(ExtendedStageWidths[x], ExtendedStageWidths[x + 1], BlocksPerStage[x], CompressionFactor, ReceptiveField, ResamplingFilter)]
         self.MainLayers = nn.ModuleList(MainLayers)
         
         self.EpilogLayer = DiscriminatorEpilogLayer(EpilogWidth, LatentDimension)
--- Train128.py
+++ Train128.py
@@ -15,18 +15,21 @@ from Networks import Generator, Discriminator
 from Loss import ZeroCenteredGradientPenalty, RelativisticLoss
 
 ema_beta = 0.998 # ffhq: 0.99778438712388889017237329703832 cifar: 0.99991128109664301904760707704894
-w_avg_beta = 0.998
-gamma = 1
+gamma = 0.5
+grad_accum = 8
+
+img_nrow = 8
+manualSeed = 42
 
 def run():
     torch.set_printoptions(threshold=1)
     logging.basicConfig(level=logging.INFO, filename='train_log.txt')
     parser = argparse.ArgumentParser()
-    parser.add_argument('--batchSize', type=int, default=6, help='input batch size')
+    parser.add_argument('--batchSize', type=int, default=64, help='input batch size')
     parser.add_argument('--imageSize', type=int, default=128, help='the height / width of the input image to network')
-    parser.add_argument('--lr', type=float, default=2e-5, help='learning rate')
-    parser.add_argument('--nz', type=int, default=512, help='size of the latent z vector')
-    parser.add_argument('--workers', type=int, help='number of data loading workers', default=2)
+    parser.add_argument('--lr', type=float, default=2e-4, help='learning rate')
+    parser.add_argument('--nz', type=int, default=64, help='size of the latent z vector')
+    parser.add_argument('--workers', type=int, help='number of data loading workers', default=8)
     parser.add_argument('--outf', default='.', help='folder to output images and model checkpoints')
     
     model = None #torch.load('epoch_36.pth', map_location='cpu')
@@ -39,8 +42,6 @@ def run():
     except OSError:
         pass
 
-    manualSeed = 42
-        
     print("Random Seed: ", manualSeed)
     random.seed(manualSeed)
     torch.manual_seed(manualSeed)
@@ -49,6 +50,8 @@ def run():
     numpy.random.seed(manualSeed)
 
     cudnn.benchmark = True
+    torch.backends.cuda.matmul.allow_tf32 = False
+    torch.backends.cudnn.allow_tf32 = False
 
     dataset = dset.ImageFolder('./Data',transform=transforms.Compose([
         transforms.Resize(opt.imageSize),
@@ -63,19 +66,16 @@ def run():
     device = torch.device("cuda:0")
     nz = int(opt.nz)
     
-    fixed_noise = torch.randn(16, nz, device=device)
+    fixed_noise = torch.randn(opt.batchSize, nz, device=device)
     
-    w_avg = torch.zeros(nz)
-    
-    netG = Generator(NoiseDimension=nz, StageWidths=[1024, 512, 512, 512, 256], BlocksPerStage=[1, 1, 1, 1, 1]).to(device)
-    netD = Discriminator(StageWidths=[256, 512, 512, 512, 1024], BlocksPerStage=[1, 1, 1, 1, 1]).to(device)
+    netG = Generator(NoiseDimension=nz, StageWidths=[1024, 512, 512, 256, 128], BlocksPerStage=[3, 3, 3, 3, 3]).to(device)
+    netD = Discriminator(StageWidths=[128, 256, 512, 512, 1024], BlocksPerStage=[3, 3, 3, 3, 3]).to(device)
     G_ema = copy.deepcopy(netG).eval()
     
     optimizerD = optim.Adam(netD.parameters(), lr=opt.lr, betas=(0, 0.99))
     optimizerG = optim.Adam(netG.parameters(), lr=opt.lr, betas=(0, 0.99))
     
     if model is not None:
-        w_avg = model['w_avg']
         fixed_noise = model['fixed_noise'].to(device)
         netG.load_state_dict(model['g_state_dict'], strict=True)
         netD.load_state_dict(model['d_state_dict'], strict=True)
@@ -85,7 +85,38 @@ def run():
      
     print('G params: ' + str(sum(p.numel() for p in netG.parameters() if p.requires_grad)))
     print('D params: ' + str(sum(p.numel() for p in netD.parameters() if p.requires_grad)))
-
+    
+    chunk_size = opt.batchSize // grad_accum
+    
+    def D_backward(real):
+        real.requires_grad = True
+        
+        noise = torch.randn(chunk_size, nz, device=device)
+        fake = netG(noise)
+        
+        output_r = netD(real)
+        output_f = netD(fake)
+        
+        r1_penalty = ZeroCenteredGradientPenalty(real, output_r)
+        r2_penalty = ZeroCenteredGradientPenalty(fake, output_f)
+        
+        errD = RelativisticLoss(output_r, output_f) + gamma * (r1_penalty + r2_penalty)
+        (errD / grad_accum).backward()
+        
+        return [x.detach().item() / grad_accum for x in [errD, r1_penalty, r2_penalty]]
+    
+    def G_backward(real):
+        noise = torch.randn(chunk_size, nz, device=device)
+        fake = netG(noise)
+        
+        output_f = netD(fake)
+        output_r = netD(real)
+        
+        errG = RelativisticLoss(output_f, output_r)
+        (errG / grad_accum).backward()
+        
+        return errG.detach().item() / grad_accum
+    
     for epoch in range(0 if model is None else model['epoch'] + 1, 1000000):
         for i, data in enumerate(dataloader, 0):
             netD.requires_grad = True
@@ -94,20 +125,17 @@ def run():
             netD.zero_grad()
             
             real = data[0][0 : opt.batchSize, :, :, :].to(device)
-            real.requires_grad = True
-
-            noise = torch.randn(opt.batchSize, nz, device=device)
-            fake = netG(noise)
             
-            output_r = netD(real)
-            output_f = netD(fake)
+            errD = 0
+            r1_penalty = 0
+            r2_penalty = 0
             
-            r1_penalty = ZeroCenteredGradientPenalty(real, output_r)
-            r2_penalty = ZeroCenteredGradientPenalty(fake, output_f)
-
-            errD = RelativisticLoss(output_r, output_f) + gamma * (r1_penalty + r2_penalty)
+            for x in range(grad_accum):
+                errD_chunk, r1_penalty_chunk, r2_penalty_chunk = D_backward(real[x * chunk_size : (x + 1) * chunk_size, :, :, :])
+                errD += errD_chunk
+                r1_penalty += r1_penalty_chunk
+                r2_penalty += r2_penalty_chunk
             
-            errD.backward()
             optimizerD.step()
 
             ###########################
@@ -119,15 +147,12 @@ def run():
             
             real = data[0][opt.batchSize : 2 * opt.batchSize, :, :, :].to(device)
             
-            noise = torch.randn(opt.batchSize, nz, device=device)
-            fake = netG(noise)
+            errG = 0
             
-            output_f = netD(fake)
-            output_r = netD(real)
-
-            errG = RelativisticLoss(output_f, output_r)
-
-            errG.backward()
+            for x in range(grad_accum):
+                errG_chunk = G_backward(real[x * chunk_size : (x + 1) * chunk_size, :, :, :])
+                errG += errG_chunk
+                
             optimizerG.step()
             
             ###########################
@@ -139,26 +164,25 @@ def run():
                     b_ema.copy_(b)
                     
             ###########################
-
-            noise = torch.randn(opt.batchSize, nz, device=device)
-            w = G_ema.LatentLayer(noise)
-            w_avg = w_avg + (1 - w_avg_beta) * (w.mean(0).detach().cpu() - w_avg)
             
-            ###########################
-            
-            log_str = '[%d][%d/%d] Loss_D: %.4f Loss_G: %.4f R1: %.4f R2: %.4f' % (epoch, i, len(dataloader), errD.detach().item(), errG.detach().item(), gamma * r1_penalty.detach().item(), gamma * r2_penalty.detach().item())
-            log_str += ' w_avg: ' + str(w_avg).removeprefix('tensor(').removesuffix(')')
+            log_str = '[%d][%d/%d] Loss_D: %.4f Loss_G: %.4f R1: %.4f R2: %.4f' % (epoch, i, len(dataloader), errD, errG, gamma * r1_penalty, gamma * r2_penalty)
             print(log_str)
             logging.info(log_str)
             
             ###########################
             
             if i % 100 == 0:
-                fake = G_ema(fixed_noise)
-                mean = G_ema(w_avg.to(device).view(1, -1), EnableLatentMapping=False)
-                vutils.save_image(real, '%s/real_samples.png' % opt.outf, normalize=True, nrow=3)
-                vutils.save_image(torch.clamp(fake.detach(), -1, 1), '%s/fake_samples_epoch_%04d_%04d.png' % (opt.outf, epoch, i), normalize=True, nrow=4)
-                vutils.save_image(torch.clamp(mean.detach(), -1, 1), '%s/mean_sample_epoch_%04d_%04d.png' % (opt.outf, epoch, i), normalize=True, nrow=1)
+                fake = []
+                with torch.no_grad():
+                    for x in range(grad_accum):
+                        fake += [G_ema(fixed_noise[x * chunk_size : (x + 1) * chunk_size, :])]
+                    noise = torch.randn(10000, nz, device=device)
+                    w = G_ema.LatentLayer(noise).mean(0).view(1, -1)
+                    mean = G_ema(w, EnableLatentMapping=False)
+                fake = torch.cat(fake, 0)
+                vutils.save_image(real, '%s/real_samples.png' % opt.outf, normalize=True, nrow=img_nrow)
+                vutils.save_image(torch.clamp(fake, -1, 1), '%s/fake_samples_epoch_%04d_%04d.png' % (opt.outf, epoch, i), normalize=True, nrow=img_nrow)
+                vutils.save_image(torch.clamp(mean, -1, 1), '%s/mean_sample_epoch_%04d_%04d.png' % (opt.outf, epoch, i), normalize=True, nrow=1)
 
         torch.save({
             'epoch': epoch,
@@ -167,12 +191,11 @@ def run():
             'd_state_dict': netD.state_dict(),
             'optimizerG_state_dict': optimizerG.state_dict(),
             'optimizerD_state_dict': optimizerD.state_dict(),
-            'w_avg': w_avg,
             'fixed_noise': fixed_noise,
-            'loss_D': errD.detach().item(),
-            'loss_G': errG.detach().item(),
-            'r1_penalty': gamma * r1_penalty.detach().item(),
-            'r2_penalty': gamma * r2_penalty.detach().item(),
+            'loss_D': errD,
+            'loss_G': errG,
+            'r1_penalty': gamma * r1_penalty,
+            'r2_penalty': gamma * r2_penalty,
             }, '%s/epoch_%d.pth' % (opt.outf, epoch))
         
 if __name__ == '__main__':
